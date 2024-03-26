ba7dc87a2e533fcd855e42894613054dc3d77614
neet2137 <66721444+Svensk2137@users.noreply.github.com>
2024-03-26 21:14:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function wastedTime() {
 
 //const screenw = window.screen.width;
 //const screenh = window.screen.height;
-const screenh = 500 - 100;
-const screenw = 750 - 160;
+const screenh = 500 - 100 - 10;
+const screenw = 750 - 160 - 10;
 let desireh = screenh;
 let desirew = screenw;
 const volvoBox = document.getElementById("volvo");
