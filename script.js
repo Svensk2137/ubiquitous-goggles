6df7ffa314de62155c397fe2be968b35f27fdb74
Ergot <kubadmidm@wp.pl>
2024-03-26 19:07:53 +0100

    these ai commit messages are kinda dumb

--- script.js
+++ script.js
@@ -4,4 +4,35 @@ function wastedTime() {
 	i = i + 1;
 }
 
+//const screenw = window.screen.width;
+//const screenh = window.screen.height;
+const screenh = 500 - 100;
+const screenw = 750 - 160;
+let desireh = screenh;
+let desirew = screenw;
+const volvoBox = document.getElementById("volvo");
+let y = 0;
+let x = 0;
+function flyVolvo() {
+	let finalY = y + "px";
+	let finalX = x + "px";
+	volvoBox.style.left = finalX;
+	volvoBox.style.top = finalY;
+	if (y < desireh) {
+		y++;
+		desireh = screenh;
+	} else {
+		y--;
+		desireh = 0;
+	}
+	if (x < desirew) {
+		x++;
+		desirew = screenw;
+	} else {
+		x--;
+		desirew = 0;
+	}
+}
+
 const timerInterval = setInterval(wastedTime, 1000);
+const flyVolvoInterval = setInterval(flyVolvo, 5);
