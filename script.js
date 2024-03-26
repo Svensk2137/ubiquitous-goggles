70ba3e209d87b478a1f179a9c997af6068ef8ee7
Ergot <kubadmidm@wp.pl>
2024-03-26 18:32:59 +0100

    penis

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let i = 1;
 function wastedTime() {
-    document.getElementById("timer").innerHTML = "wasted time: " + i + " sec";
-    i = i + 1;
+	document.getElementById("timer").innerHTML = "wasted time: " + i + " sec";
+	i = i + 1;
 }
 
 const timerInterval = setInterval(wastedTime, 1000);
