288e2cdea57d996ecf063b5e673492f31eb9420e
Ergot <kubadmidm@wp.pl>
2024-03-26 18:16:02 +0100

    ye

---
+++ script.js
@@ -0,0 +1,8 @@
+function wastedTime() {
+    let i = 0;
+    var timer = document.getElementById("timer").innerHTML;
+    timer = i + " sec";
+    i++;
+}
+
+const timerInterval = setInterval(wastedTime, 1000);
