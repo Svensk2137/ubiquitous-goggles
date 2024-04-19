5a01b3d3314baf65c82e3a2f1ba66cbe319cb6f1
Svensk2137 <kubadmidm@wp.pl>
2024-04-19 15:57:05 +0200

    pepe

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-let i = 1;
-function wastedTime() {
-	document.getElementById("timer").innerHTML = "wasted time: " + i + " sec";
-	document.title = "Penis"+i;
-	i = i + 1;
+function sex() {
+	var myWindow = window.open("", "MsgWindow", "width=220,height=220");
+	myWindow.document.write("<img width=200 height=200 src=\"https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png\"> <script src=\"windowscript.js\"></script>");
 }
-
-const timerInterval = setInterval(wastedTime, 1000);
